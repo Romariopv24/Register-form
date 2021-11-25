46658d1dc0b675fca50420caa41af00ad90b79b6
Rom <laloromario95@gmail.com>
2021-11-25 18:30:09 -0400

    Mi primer commit real

---
+++ main.js
@@ -0,0 +1,30 @@
+const loginIn = document.getElementById('login-in'),
+      loginUp = document.getElementById('login-up'),
+      signUp = document.getElementById('sign-up'),
+      signIn = document.getElementById('sign-in'),
+      usuario = document.getElementById('username'),
+      email = document.getElementById('email'),
+      password = document.getElementById('password');
+
+  
+    signUp.addEventListener('click' , () => {
+
+        //Revover las clases si existen
+        loginIn.classList.remove('block');
+        loginUp.classList.remove('none');
+
+        //Agregar las clases
+        loginIn.classList.toggle('none');
+        loginUp.classList.toggle('block');
+    });
+
+    signIn.addEventListener('click' , () => {
+
+        //Revover las clases si existen
+        loginIn.classList.remove('none');
+        loginUp.classList.remove('block');
+
+        //Agregar las clases
+        loginIn.classList.toggle('block');
+        loginUp.classList.toggle('none');
+    })
